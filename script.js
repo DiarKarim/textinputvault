f0377d67eabf03db653fe072f1de0580d0dc741e
Diar Karim <diarkarim@gmail.com>
2024-02-25 19:29:09 +0000

    styles

--- script.js
+++ script.js
@@ -7,15 +7,10 @@ fetch(`${mediaFolder}/media_list.json`)
     data.forEach(item => {
       const galleryItem = document.createElement('div');
       galleryItem.classList.add('gallery-item');
-      galleryItem.style.width = '210px'; 
-      galleryItem.style.height = '150px'; // Title height (50px) + Image Height (100px) 
 
       const mediaElement = document.createElement(item.url.endsWith('.gif') ? 'img' : 'video');
       mediaElement.src = `${mediaFolder}/${item.url}`;
       mediaElement.alt = item.title; 
-      mediaElement.style.width = '210px';
-      mediaElement.style.height = '100px';
-      mediaElement.style.objectFit = 'cover'; // For cropping
 
       const titleOverlay = document.createElement('div');
       titleOverlay.classList.add('title-overlay');
